ecf20e749115891871e04ebb188ff9c340689227
Michal Elias <michal1.elias@t-mobile.cz>
2023-08-30 15:46:29 +0200

    Initial commit

---
+++ js/user-greeting.js
@@ -0,0 +1,13 @@
+
+let firstName = prompt("what's your name: ");
+let surname = prompt("what is your surname: ");
+let age = prompt("what is your age: ");
+
+console.log("First name: ", firstName, " Last name: ", surname, " age:", age);
+
+
+console.log("Last name: ", surname);
+console.log(" age:", age);
+console.log("First name: ", firstName, " Last name: ", surname, " age:", age);
+
+alert(`Ahoj ${firstName}`)
